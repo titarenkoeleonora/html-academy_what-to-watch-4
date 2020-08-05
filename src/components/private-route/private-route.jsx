2f1ebf34b1ef6ea07dfd3964d4dbb75e0885ed89
EleonoraTitarenko <yaroslavnaella@gmail.com>
2020-08-06 01:31:44 +0300

    Fix PrivateRoute bug

--- src/components/private-route/private-route.jsx
+++ src/components/private-route/private-route.jsx
@@ -13,10 +13,10 @@ const PrivateRoute = (props) => {
     <Route
       path={path}
       exact={exact}
-      render={() => {
+      render={(routeProps) => {
         return (
           authorizationStatus !== AuthorizationStatus.NO_AUTH
-            ? render()
+            ? render(routeProps)
             : <Redirect to={AppRoute.LOGIN} />
         );
       }}
